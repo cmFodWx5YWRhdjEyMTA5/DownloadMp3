3ff7f4b421072b2bc0d2fc6e4946073e4197e771
liyanju <liyanju@baidu.com>
2018-07-21 17:16:11 +0800

    splash 页面

--- app/src/main/java/com/freedownloader/ui/SplashActivity.java
+++ app/src/main/java/com/freedownloader/ui/SplashActivity.java
@@ -1,12 +1,24 @@
 package com.freedownloader.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 import com.freedownloader.R;
+import com.freedownloader.util.Utils;
+
+import java.lang.reflect.Method;
 
 
 /**
@@ -28,7 +40,38 @@ public class SplashActivity extends AppCompatActivity{
 
         setContentView(R.layout.splash_activity);
 
+        FrameLayout frameLayout = findViewById(R.id.splash_container);
+
+        try {
+            int height = getVirtualBarHeigh();
+            if (height > 0) {
+                FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(frameLayout.getLayoutParams());
+                lp.setMargins(0, height, 0, 0);
+                frameLayout.setLayoutParams(lp);
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
         initViews();
+    }
+
+    public int getVirtualBarHeigh() {
+        int vh = 0;
+        WindowManager windowManager = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics dm = new DisplayMetrics();
+        try {
+            @SuppressWarnings("rawtypes")
+            Class c = Class.forName("android.view.Display");
+            @SuppressWarnings("unchecked")
+            Method method = c.getMethod("getRealMetrics", DisplayMetrics.class);
+            method.invoke(display, dm);
+            vh = dm.heightPixels - windowManager.getDefaultDisplay().getHeight();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return vh;
     }
 
     private void initViews() {

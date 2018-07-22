e01dde8df3c93c094c2c7883b4a104d2077e2b5c
liyanju <liyanju@baidu.com>
2018-07-22 15:09:05 +0800

    更换FB ID

--- app/src/main/java/com/freedownloader/util/Constants.java
+++ app/src/main/java/com/freedownloader/util/Constants.java
@@ -12,9 +12,15 @@ public class Constants {
 
     public static final String KEY_SING_YOUTUBE = "single_ytb";
 
-    public static final String CHA_YE_ID = "244665246152799_244666539486003";
-    public static final String CHA_YE_HEIGH_ID = "244665246152799_244666632819327";
+//    public static final String CHA_YE_ID = "244665246152799_244666539486003";
+//    public static final String CHA_YE_HEIGH_ID = "244665246152799_244666632819327";
+//
+//    public static final String NATIVE_ID_LIST = "244665246152799_244666369486020";
+//    public static final String NATIVE_ID_DIALOG = "244665246152799_244666096152714";
 
-    public static final String NATIVE_ID_LIST = "244665246152799_244666369486020";
-    public static final String NATIVE_ID_DIALOG = "244665246152799_244666096152714";
+    public static final String CHA_YE_ID = "2126711137570479_2126712340903692";
+    public static final String CHA_YE_HEIGH_ID = "2126711137570479_2126712224237037";
+
+    public static final String NATIVE_ID_LIST = "2126711137570479_2126711964237063";
+    public static final String NATIVE_ID_DIALOG = "2126711137570479_2126711727570420";
 }

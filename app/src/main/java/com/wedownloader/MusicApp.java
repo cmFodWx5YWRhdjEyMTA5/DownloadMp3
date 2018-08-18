a9d45786b0788ef76151ce2f46b4c60adcb32e9c
liyanju <liyanju@baidu.com>
2018-08-18 11:54:47 +0800

    realse 1.0.1

--- app/src/main/java/com/wedownloader/MusicApp.java
+++ app/src/main/java/com/wedownloader/MusicApp.java
@@ -74,7 +74,6 @@ public class MusicApp extends Application {
         FBAdUtils.init(this);
         FBAdUtils.loadFBAds(Constants.NATIVE_ID_LIST);
 
-        ReferrerHandler.setSingytb();
         ReferrerHandler.initReferrer();
 
         if (!MusicApp.sPreferences.getBoolean("new_short_cut", false)) {
